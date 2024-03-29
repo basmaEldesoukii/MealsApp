b03302ac3a25d641794e0497bb49553ea31b243d
Basma Eldesouky <41978905+basmaEldesouky@users.noreply.github.com>
2024-03-29 17:22:15 +0200

    add common utils classes in common module

---
+++ common/src/main/java/com/basma/common/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.basma.common.utils
+
+class Constants {
+    companion object {
+
+    }
+}
---
+++ common/src/main/java/com/basma/common/utils/Resource.kt
@@ -0,0 +1,9 @@
+package com.basma.common.utils
+
+import java.lang.Exception
+
+sealed class Resource<out T> {
+    data object Loading: Resource<Nothing>()
+    class Success<T>(val data: T): Resource<T>()
+    class Error(val exception: Exception): Resource<Nothing>()
+}
