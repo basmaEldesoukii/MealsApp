7b93bda9dee1317ef47d14320cefe3771d5e2a46
Basma Eldesouky <41978905+basmaEldesouky@users.noreply.github.com>
2024-03-30 05:36:44 +0200

    add meals_list repo contract in domain module

---
+++ meals_list/src/main/java/com/basma/meals_list/domain/contract/MealsListRepositoryContract.kt
@@ -0,0 +1,9 @@
+package com.basma.meals_list.domain.contract
+
+import com.basma.common.utils.Resource
+import com.basma.meals_list.domain.entity.Meal
+import kotlinx.coroutines.flow.Flow
+
+interface MealsListRepositoryContract {
+    suspend fun getMealsList(): Flow<Resource<List<Meal>>>
+}
