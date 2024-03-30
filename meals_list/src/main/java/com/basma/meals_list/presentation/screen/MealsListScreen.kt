d4ff04d59c7b44974630120195cfd2e707dd3d08
Basma Eldesouky <41978905+basmaEldesouky@users.noreply.github.com>
2024-03-30 18:19:31 +0200

    feat: create initial Meals list screen for mealsList ui

---
+++ meals_list/src/main/java/com/basma/meals_list/presentation/screen/MealsListScreen.kt
@@ -0,0 +1,49 @@
+package com.basma.meals_list.presentation.screen
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.basma.meals_list.presentation.component.CategoryBanner
+import com.basma.meals_list.presentation.component.MealsListSection
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MealsListScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("") },
+                navigationIcon = {
+                    IconButton(onClick = { /*navController.popBackStack()*/ }) {
+                        Icon(Icons.Filled.ArrowBack, contentDescription = "Back to homepage")
+                    }
+                }
+            )
+        }
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            CategoryBanner()
+            Spacer(modifier = Modifier.height(16.dp))
+            MealsListSection()
+        }
+    }
+}
