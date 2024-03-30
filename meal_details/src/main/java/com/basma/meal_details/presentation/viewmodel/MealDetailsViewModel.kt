980d85bb4408a741253d9ae869c3cf9d9fc7dd8b
Basma Eldesouky <41978905+basmaEldesouky@users.noreply.github.com>
2024-03-30 21:18:21 +0200

    feat: create Meal details viewmodel

---
+++ meal_details/src/main/java/com/basma/meal_details/presentation/viewmodel/MealDetailsViewModel.kt
@@ -0,0 +1,63 @@
+package com.basma.meal_details.presentation.viewmodel
+
+import androidx.lifecycle.viewModelScope
+import com.basma.base.BaseViewModel
+import com.basma.common.utils.Resource
+import com.basma.meal_details.domain.usecase.GetMealDetailsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MealDetailsViewModel @Inject constructor(
+    private val getMealDetailsUseCase: GetMealDetailsUseCase
+) : BaseViewModel<MealDetailsContract.MealDetailsIntent, MealDetailsContract.MealDetailsState>() {
+
+    override fun createInitialState(): MealDetailsContract.MealDetailsState {
+        return MealDetailsContract.MealDetailsState(MealDetailsContract.MealDetailsDataState.Loading)
+    }
+
+    override fun handleIntent(intent: MealDetailsContract.MealDetailsIntent) {
+        when (intent) {
+            is MealDetailsContract.MealDetailsIntent.OnFetchMealDetails -> {
+                fetchMealDetails(intent.mealID)
+            }
+        }
+    }
+
+    private fun fetchMealDetails(mealID: Int) {
+        viewModelScope.launch(Dispatchers.IO) {
+            getMealDetailsUseCase.invoke(mealID)
+                .onStart { emit(Resource.Loading) }
+                .collect {
+                    when (it) {
+                        is Resource.Loading -> {
+                            setState { copy(mealDetailsState = MealDetailsContract.MealDetailsDataState.Loading) }
+                        }
+
+                        is Resource.Success -> {
+                            setState {
+                                copy(
+                                    mealDetailsState = MealDetailsContract.MealDetailsDataState.Success(
+                                        it.data
+                                    )
+                                )
+                            }
+                        }
+
+                        is Resource.Error -> {
+                            setState {
+                                copy(
+                                    mealDetailsState = MealDetailsContract.MealDetailsDataState.Error(
+                                        it.exception.localizedMessage
+                                    )
+                                )
+                            }
+                        }
+                    }
+                }
+        }
+    }
+}
