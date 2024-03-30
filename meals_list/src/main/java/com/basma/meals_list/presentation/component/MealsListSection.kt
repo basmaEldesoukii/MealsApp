ffefd65c8777bf004e0550ee54efb4415f3f4822
Basma Eldesouky <41978905+basmaEldesouky@users.noreply.github.com>
2024-03-30 19:57:49 +0200

    feat: create initial MealsListSection component for mealsList ui

---
+++ meals_list/src/main/java/com/basma/meals_list/presentation/component/MealsListSection.kt
@@ -0,0 +1,22 @@
+package com.basma.meals_list.presentation.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MealsListSection() {
+    LazyColumn(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        //TODO add mealsList
+        /*items(meals) { meal ->
+            MealsListItem()
+        }*/
+    }
+}
