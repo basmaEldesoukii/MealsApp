7f27d3a6437ccb63830218c551bd2cf6d940284d
Basma Eldesouky <41978905+basmaEldesouky@users.noreply.github.com>
2024-03-30 16:45:33 +0200

    feat: create initial CategoriesSectionComponent for homepage ui

---
+++ homepage/src/main/java/com/basma/homepage/presentation/component/CategoriesSection.kt
@@ -0,0 +1,21 @@
+package com.basma.homepage.presentation.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CategoriesSection() {
+    LazyRow(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        //TODO ADD categories list
+        /*items(categories) { category ->
+            CategoryItem()
+        }*/
+    }
+}
