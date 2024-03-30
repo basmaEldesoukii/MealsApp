745f81fe93747539e4cf98c9a1bf3306053137cf
Basma Eldesouky <41978905+basmaEldesouky@users.noreply.github.com>
2024-03-30 17:02:48 +0200

    feat: create initial HomePage Screen for homepage ui

---
+++ homepage/src/main/java/com/basma/homepage/presentation/screen/HomePageScreen.kt
@@ -0,0 +1,43 @@
+package com.basma.homepage.presentation.screen
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.basma.homepage.presentation.component.CategoriesSection
+import com.basma.homepage.presentation.component.HighlightsSection
+import com.basma.homepage.presentation.component.IngredientsSection
+import com.basma.homepage.presentation.component.MealBanner
+import com.basma.homepage.presentation.component.MealsSection
+import com.basma.homepage.presentation.component.TitleHeader
+
+@Composable
+fun HomePageScreen() {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        item {
+            MealBanner()
+        }
+        item {
+            TitleHeader(title = "")
+            CategoriesSection()
+        }
+        item {
+            TitleHeader(title = "")
+            MealsSection()
+        }
+        item {
+            TitleHeader(title = "")
+            IngredientsSection()
+        }
+        item {
+            TitleHeader(title = "")
+            HighlightsSection()
+        }
+    }
+}
