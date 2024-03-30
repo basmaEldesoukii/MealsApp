9c552b1b538fc9c16a2d649bda15016322abb85b
Basma Eldesouky <41978905+basmaEldesouky@users.noreply.github.com>
2024-03-30 05:36:30 +0200

    add meals_list entities in domain module

---
+++ meals_list/src/main/java/com/basma/meals_list/domain/entity/Meal.kt
@@ -0,0 +1,7 @@
+package com.basma.meals_list.domain.entity
+
+data class Meal(
+    val idMeal: String,
+    val strMeal: String,
+    val strMealThumb: String
+)
---
+++ meals_list/src/main/java/com/basma/meals_list/domain/entity/MealsListDataModel.kt
@@ -0,0 +1,5 @@
+package com.basma.meals_list.domain.entity
+
+data class MealsListDataModel(
+    val meals: List<Meal>
+)
