51f64d34e91b5bbd21426d28f54223ed9a54b3a0
Basma Eldesouky <41978905+basmaEldesouky@users.noreply.github.com>
2024-03-30 16:57:10 +0200

    feat: create initial MealsSectionComponent for homepage ui

---
+++ homepage/src/main/java/com/basma/homepage/presentation/component/MealsSection.kt
@@ -0,0 +1,18 @@
+package com.basma.homepage.presentation.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MealsSection() {
+    LazyRow(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        //TODO ADD meals list
+        /*items(meals) { meal ->
+            MealItem()
+        }*/
+    }
+}
