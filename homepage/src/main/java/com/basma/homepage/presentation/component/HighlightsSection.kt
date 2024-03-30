1614a903cc7b6a3256b0a1bdeb2791ccf42f1ef6
Basma Eldesouky <41978905+basmaEldesouky@users.noreply.github.com>
2024-03-30 17:02:36 +0200

    feat: create initial HighlightsSection Component for homepage ui

---
+++ homepage/src/main/java/com/basma/homepage/presentation/component/HighlightsSection.kt
@@ -0,0 +1,18 @@
+package com.basma.homepage.presentation.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HighlightsSection() {
+    LazyRow(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        //TODO ADD highlights list
+        /*items(highlights) { highlight ->
+            HighlightItem(highlight = highlight)
+        }*/
+    }
+}
