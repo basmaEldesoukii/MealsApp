841bd018d4371a0255aa56b769318576cef3304f
Basma Eldesouky <41978905+basmaEldesouky@users.noreply.github.com>
2024-04-01 05:43:52 +0200

    test: create homepage local data source test

---
+++ homepage/src/test/java/com/basma/homepage/data/HomePageLocalDataSourceImpUnitTest.kt
@@ -0,0 +1,125 @@
+package com.basma.homepage.data
+
+import androidx.test.filters.SmallTest
+import com.basma.homepage.data.local.HomePageDao
+import com.basma.homepage.data.local.HomePageLocalDataSourceImp
+import com.basma.homepage.utils.TestDataGenerator
+import com.google.common.truth.Truth
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import java.lang.Exception
+
+@ExperimentalCoroutinesApi
+@SmallTest
+class HomePageLocalDataSourceImpUnitTest {
+    @MockK
+    private lateinit var homePageDao: HomePageDao
+
+    private lateinit var localDataSourceImp: HomePageLocalDataSourceImp
+
+    @Before
+    fun setup(){
+        MockKAnnotations.init(this, relaxUnitFun = true)
+        localDataSourceImp = HomePageLocalDataSourceImp(
+            homePageDao = homePageDao
+        )
+    }
+
+    @Test
+    fun test_get_homepage_data_success() = runTest {
+        val localData = TestDataGenerator.generateHomePageLocalData()
+
+        // Given
+        coEvery { homePageDao.getHomePageData() } returns localData
+
+        // When
+        val returned = localDataSourceImp.getHomePageDataFromDataBase()
+
+        // Then
+        coVerify { homePageDao.getHomePageData() }
+
+        // Assertion
+        Truth.assertThat(returned).isEqualTo(localData)
+    }
+
+    @Test(expected = Exception::class)
+    fun test_get_homepage_data_fail() = runTest {
+        // Given
+        coEvery { homePageDao.getHomePageData() } throws Exception()
+
+        // When
+        localDataSourceImp.getHomePageDataFromDataBase()
+
+        // Then
+        coVerify { homePageDao.getHomePageData() }
+    }
+
+    @Test
+    fun test_insert_homepage_data_success() = runTest {
+        val localData = TestDataGenerator.generateHomePageLocalData()
+        val expected = 1L
+
+        // Given
+        coEvery { homePageDao.insertHomePageData(localData) } returns expected
+
+        // When
+        val returned = localDataSourceImp.insertHomePageDataList(localData)
+
+        // Then
+        coVerify { homePageDao.insertHomePageData(any()) }
+
+        // Assertion
+        Truth.assertThat(returned).isEqualTo(expected)
+    }
+
+    @Test(expected = Exception::class)
+    fun test_insert_homepage_data_fail() = runTest {
+        val localData = TestDataGenerator.generateHomePageLocalData()
+
+        // Given
+        coEvery { homePageDao.getHomePageData() } throws Exception()
+
+        // When
+        localDataSourceImp.insertHomePageDataList(localData)
+
+        // Then
+        coVerify { homePageDao.insertHomePageData(any()) }
+    }
+
+
+    @Test
+    fun test_clear_all_homepage_data_success() = runTest {
+        val localData = TestDataGenerator.generateHomePageLocalData()
+        val expected = localData.id // Affected row count
+
+        // Given
+        coEvery { homePageDao.clearHomePageDataCash() } returns expected.toInt()
+
+        // When
+        val returned = localDataSourceImp.clearHomePageDataCashed()
+
+        // Then
+        coVerify { homePageDao.clearHomePageDataCash() }
+
+        // Assertion
+        Truth.assertThat(returned).isEqualTo(expected)
+    }
+
+    @Test(expected = Exception::class)
+    fun test_clear_all_homepage_data_fail() = runTest {
+        // Given
+        coEvery { homePageDao.clearHomePageDataCash() } throws Exception()
+
+        // When
+        localDataSourceImp.clearHomePageDataCashed()
+
+        // Then
+        coVerify { homePageDao.clearHomePageDataCash() }
+    }
+}
--- homepage/src/test/java/com/basma/homepage/utils/TestDataGenerator.kt
+++ homepage/src/test/java/com/basma/homepage/utils/TestDataGenerator.kt
@@ -1,5 +1,6 @@
 package com.basma.homepage.utils
 
+import com.basma.homepage.data.local.HomePageLocalEntity
 import com.basma.homepage.domain.entity.Category
 import com.basma.homepage.domain.entity.Data
 import com.basma.homepage.domain.entity.DynamicCollectionViewModel
@@ -8,39 +9,53 @@ import com.basma.homepage.domain.entity.HomePageDataType
 
 class TestDataGenerator {
     companion object{
+        val dataMock = Data(
+            DynamicCollectionViewModel = listOf(
+                DynamicCollectionViewModel(
+                    Type = HomePageDataType.Category,
+                    Title = "Top Categories",
+                    Order = 3,
+                    Categories = listOf(
+                        Category(
+                            idCategory = "1",
+                            strCategory = "Mock Beef",
+                            strCategoryThumb = "https://www.example.com/beef.png",
+                            strCategoryDescription = "Mock description for beef"
+                        )
+                    ),
+                    Announcements = listOf(),
+                    Ingredients = listOf(),
+                    Id = 1,
+                    Meals = listOf(),
+                    Url = "https://www.example.com"
+                )
+            )
+        )
+
         // Data for UseCase Test
         fun generateHomePageData(): HomePageDataModel {
             return generateDataItem()
         }
 
-        fun generateDataItem(): HomePageDataModel {
+        private fun generateDataItem(): HomePageDataModel {
             return HomePageDataModel(
-                Data = Data(
-                    DynamicCollectionViewModel = listOf(
-                        DynamicCollectionViewModel(
-                            Type = HomePageDataType.Category,
-                            Title = "Top Categories",
-                            Order = 3,
-                            Categories = listOf(
-                                Category(
-                                    idCategory = "1",
-                                    strCategory = "Mock Beef",
-                                    strCategoryThumb = "https://www.example.com/beef.png",
-                                    strCategoryDescription = "Mock description for beef"
-                                )
-                            ),
-                            Announcements = listOf(),
-                            Ingredients = listOf(),
-                            Id = 1,
-                            Meals = listOf(),
-                            Url = "https://www.example.com"
-                        )
-                    )
-                )
+                Data = dataMock
             )
         }
         //endOfRegion
 
+        //Data for LocalData Test
+        fun generateHomePageLocalData(): HomePageLocalEntity {
+            return generateHomePageLocalItem()
+        }
+
+        private fun generateHomePageLocalItem(): HomePageLocalEntity {
+            return HomePageLocalEntity(
+                id = 0,
+                data = dataMock
+            )
+        }
+        //endOfRegion
 
     }
 }
