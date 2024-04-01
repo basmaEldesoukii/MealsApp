6088aff98c378dcdcfafb9bde2cb18e7e12b84b0
Basma Eldesouky <41978905+basmaEldesouky@users.noreply.github.com>
2024-04-01 07:25:28 +0200

    test: create mealDetails local data source test

---
+++ meal_details/src/test/java/com/basma/meal_details/data/MealDetailsLocalDataSourceImpUnitTest.kt
@@ -0,0 +1,127 @@
+package com.basma.meal_details.data
+
+import androidx.test.filters.SmallTest
+import com.basma.meal_details.data.local.MealDetailsDao
+import com.basma.meal_details.data.local.MealDetailsLocalDataSourceImp
+import com.basma.meal_details.utils.TestDataGenerator
+import com.google.common.truth.Truth
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import java.lang.Exception
+
+@ExperimentalCoroutinesApi
+@SmallTest
+class MealDetailsLocalDataSourceImpUnitTest {
+    @MockK
+    private lateinit var mealDetailsDao: MealDetailsDao
+
+    private lateinit var localDataSourceImp: MealDetailsLocalDataSourceImp
+
+    @Before
+    fun setup(){
+        MockKAnnotations.init(this, relaxUnitFun = true)
+        localDataSourceImp = MealDetailsLocalDataSourceImp(
+            mealDetailsDao = mealDetailsDao
+        )
+    }
+
+    @Test
+    fun test_get_meal_details_success() = runTest {
+        val expectedMealDetails = TestDataGenerator.generateMealDetailsLocalData()
+
+        // Given
+        coEvery { mealDetailsDao.getMealDetails(any()) } returns expectedMealDetails
+
+        // When
+        val returned = localDataSourceImp.getMealDetailsFromDataBase(expectedMealDetails.idMeal)
+
+        // Then
+        coVerify { mealDetailsDao.getMealDetails(any()) }
+
+        // Assertion
+        Truth.assertThat(returned).isEqualTo(expectedMealDetails)
+    }
+
+    @Test(expected = Exception::class)
+    fun test_get_meal_details_fail() = runTest {
+        val mealDetails = TestDataGenerator.generateMealDetailsLocalData()
+
+        // Given
+        coEvery { mealDetailsDao.getMealDetails(any()) } throws Exception()
+
+        // When
+        localDataSourceImp.getMealDetailsFromDataBase(mealDetails.idMeal)
+
+        // Then
+        coVerify { mealDetailsDao.getMealDetails(any()) }
+    }
+
+
+    @Test
+    fun test_insert_meal_details_success() = runTest {
+        val localMeals = TestDataGenerator.generateMealDetailsLocalData()
+        val expected = 1L
+
+        // Given
+        coEvery { mealDetailsDao.insertMealDetails(any()) } returns expected
+
+        // When
+        val returned = localDataSourceImp.insertMealDetails(localMeals)
+
+        // Then
+        coVerify { mealDetailsDao.insertMealDetails(any()) }
+
+        // Assertion
+        Truth.assertThat(returned).isEqualTo(expected)
+    }
+
+    @Test(expected = Exception::class)
+    fun test_insert_meal_details_fail() = runTest {
+        val localMeals = TestDataGenerator.generateMealDetailsLocalData()
+
+        // Given
+        coEvery { mealDetailsDao.insertMealDetails(any()) } throws Exception()
+
+        // When
+        localDataSourceImp.insertMealDetails(localMeals)
+
+        // Then
+        coVerify { mealDetailsDao.insertMealDetails(any()) }
+    }
+
+    @Test
+    fun test_clear_all_meal_details_success() = runTest {
+        val localMeals = TestDataGenerator.generateListOfMealDetailsItem()
+        val expected = localMeals.mealDetails.size // Affected row count
+
+        // Given
+        coEvery { mealDetailsDao.clearMealDetailsCash() } returns expected
+
+        // When
+        val returned = localDataSourceImp.clearMealDetailsCashed()
+
+        // Then
+        coVerify { mealDetailsDao.clearMealDetailsCash() }
+
+        // Assertion
+        Truth.assertThat(returned).isEqualTo(expected)
+    }
+
+    @Test(expected = Exception::class)
+    fun test_clear_all_meal_details_fail() = runTest {
+        // Given
+        coEvery { mealDetailsDao.clearMealDetailsCash() } throws Exception()
+
+        // When
+        localDataSourceImp.clearMealDetailsCashed()
+
+        // Then
+        coVerify { mealDetailsDao.clearMealDetailsCash() }
+    }
+}
--- meal_details/src/test/java/com/basma/meal_details/utils/TestDataGenerator.kt
+++ meal_details/src/test/java/com/basma/meal_details/utils/TestDataGenerator.kt
@@ -1,5 +1,6 @@
 package com.basma.meal_details.utils
 
+import com.basma.meal_details.data.local.MealDetailsLocalEntity
 import com.basma.meal_details.domain.entity.MealDetails
 import com.basma.meal_details.domain.entity.MealDetailsDataModel
 
@@ -61,6 +62,52 @@ class TestDataGenerator {
                 strTags = "Mock, Tags, Here",
                 strYoutube = "https://www.youtube.com/watch?v=mockvideo"
             )
+        }
+        //endOfRegion
+
+        //Data for LocalData Test
+        fun generateMealDetailsLocalData(): MealDetailsLocalEntity {
+            return MealDetailsLocalEntity(
+                idMeal = "1",
+                strArea = "Mock Area",
+                strCategory = "Mock Category",
+                strIngredient1 = "Ingredient 1",
+                strIngredient10 = "Ingredient 10",
+                strIngredient11 = "Ingredient 11",
+                strIngredient12 = "Ingredient 12",
+                strIngredient13 = "Ingredient 13",
+                strIngredient14 = "Ingredient 14",
+                strIngredient15 = "Ingredient 15",
+                strIngredient2 = "Ingredient 2",
+                strIngredient3 = "Ingredient 3",
+                strIngredient4 = "Ingredient 4",
+                strIngredient5 = "Ingredient 5",
+                strIngredient6 = "Ingredient 6",
+                strIngredient7 = "Ingredient 7",
+                strIngredient8 = "Ingredient 8",
+                strIngredient9 = "Ingredient 9",
+                strInstructions = "Mock instructions for preparing the meal.",
+                strMeal = "Mock Meal",
+                strMealThumb = "https://www.example.com/mock_meal.jpg",
+                strMeasure1 = "1 cup",
+                strMeasure10 = "10 grams",
+                strMeasure11 = "11 grams",
+                strMeasure12 = "12 grams",
+                strMeasure13 = "13 grams",
+                strMeasure14 = "14 grams",
+                strMeasure15 = "15 grams",
+                strMeasure2 = "2 tablespoons",
+                strMeasure3 = "3 teaspoons",
+                strMeasure4 = "4 cups",
+                strMeasure5 = "5 slices",
+                strMeasure6 = "6 pieces",
+                strMeasure7 = "7 tablespoons",
+                strMeasure8 = "8 teaspoons",
+                strMeasure9 = "9 cups",
+                strTags = "Mock, Tags, Here",
+                strYoutube = "https://www.youtube.com/watch?v=mockvideo"
+            )
+
         }
         //endOfRegion
     }
